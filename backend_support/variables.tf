7b472a26265295922d3f16e9a03bd660aef7bcb3
luisalvarez246 <luis.alvarez1@thoughtworks.com>
2026-03-22 14:06:32 +0100

    fix(SESS-3): add default values to backend_support/variables.tf

--- backend_support/variables.tf
+++ backend_support/variables.tf
@@ -1,9 +1,11 @@
 variable "prefix" {
   type        = string
   description = "Short, unique lowercase prefix for resource names."
+  default = "lcay-iac-lab"
 }
 
 variable "region" {
   type        = string
   description = "AWS region to deploy resources (e.g. us-east-1)."
+  default     = "eu-central-1"
 }
