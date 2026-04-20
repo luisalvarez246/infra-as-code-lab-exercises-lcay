f3631b896e2c8c590490e9d4a41ae66229d5263c
luisalvarez246 <luis.alvarez1@thoughtworks.com>
2026-04-20 19:24:01 +0200

    refactor(SESS-6): remove unused subnet CIDR variables from Terraform configuration

--- variables.tf
+++ variables.tf
@@ -13,31 +13,6 @@ variable "vpc_cidr" {
   description = "VPC CIDR block"
 }
 
-variable "subnet1_cidr" {
-  type        = string
-  description = "CIDR block for subnet 1 (e.g. 10.0.1.0/24)."
-}
-variable "subnet2_cidr" {
-  type        = string
-  description = "CIDR block for subnet 2 (e.g. 10.0.2.0/24)."
-}
-variable "subnet3_cidr" {
-  type        = string
-  description = "CIDR block for subnet 3 (e.g. 10.0.3.0/24)."
-}
-variable "subnet4_cidr" {
-  type        = string
-  description = "CIDR block for subnet 4 (e.g. 10.0.4.0/24)."
-}
-variable "subnet5_cidr" {
-  type        = string
-  description = "CIDR block for subnet 5 (e.g. 10.0.5.0/24)."
-}
-variable "subnet6_cidr" {
-  type        = string
-  description = "CIDR block for subnet 6 (e.g. 10.0.6.0/24)."
-}
-
 variable "number_of_public_subnets" {
   type        = number
   description = "Number of public subnets to create."
